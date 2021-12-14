a25882406b5ffadce785f2cdb03a2e35b976640e
Donald Morales <morales.donald1025@gmail.com>
2021-12-14 14:06:16 +0800

    testing

--- controllers/user.js
+++ controllers/user.js
@@ -224,16 +224,15 @@ return User.findByIdAndUpdate(reqParams.userId, setAsAdmin).then((user, error) =
 
 
 
-module.exports.setAsAdmin = (reqParams, reqBody, userData) => {
+module.exports.setAsAdmin = (reqParams, userData) => {
 	
 	return User.findById(userData.userId).then(result => {
 		if(userData.isAdmin == false){
 			return "You are not an admin can't assign another user as Admin"
 		}else {
-			return "success"
-		/*	return User.findById(req.params.userId).then(result => {
+			return User.findById(req.params.userId).then(result => {
 				return result.isAdmin = true
-			})*/
+			})
 		}
 	})
 
--- routes/user.js
+++ routes/user.js
@@ -76,8 +76,7 @@ router.put("/:userId/setAsAdmin", auth.verify, (req, res) => {
 
 const userData = auth.decode(req.headers.authorization)
 
-	userController.setAsAdmin(req.params, {userData: userData.id, isAdmin:userData.isAdmin}).then(resultFromController => res.send(resultFromController))
-})
+	userController.setAsAdmin(req.params, userData).then(result => res.send(result));
 
 
 
